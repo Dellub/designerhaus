12772406e7d24c33890d035b3979b75940bc29f4
Rodrigo Godoy <rodrigo.godoy@dellub.com>
2025-01-21 01:27:00 -0300

    chore: finish setup

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(process.env.NESTJS_API_PORT ?? 8080)
+  await app.listen(process.env.NESTJS_API_PORT ?? 8080, () => {
+    console.log('API listening on port 8080')
+  })
 }
 
 bootstrap()
