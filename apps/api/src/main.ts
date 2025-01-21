4780104df02fffb2f14b94086474b390ff2a7302
Rodrigo Godoy <rodrigo.godoy@dellub.com>
2025-01-20 22:23:31 -0300

    chore: removed eslint and prettier, and added biomejs

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,9 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.NESTJS_API_PORT ?? 8080);
+  const app = await NestFactory.create(AppModule)
+  await app.listen(process.env.NESTJS_API_PORT ?? 8080)
 }
 
-bootstrap();
+bootstrap()
