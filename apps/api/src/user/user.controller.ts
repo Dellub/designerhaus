ec1ce224f5224c6f77df1a3af4403042df9137d4
Rodrigo Godoy <rodrigo.godoy@dellub.com>
2025-03-08 12:53:09 -0300

    feat: finish adjust ui and config

--- apps/api/src/user/user.controller.ts
+++ apps/api/src/user/user.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common'
-import type { UserService } from './user.service'
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common'
 import type { CreateUserDto } from './dto/create-user.dto'
 import type { UpdateUserDto } from './dto/update-user.dto'
+import type { UserService } from './user.service'
 
 @Controller('user')
 export class UserController {
